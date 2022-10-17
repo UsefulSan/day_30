0b744ce59b5bc9fc86a4abaab7038a3b6e7038d4
Aleksandr N <usefulsan@yandex.ru>
2022-10-17 23:48:17 +0500

    add func read_file

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import csv
+
+from table_creation import *
 
 
+def read_file():
+    with open('main_animals.csv', newline='', encoding='utf-8') as file:
+        list_data = []
+        file_reader = csv.DictReader(file)
+        for row in file_reader:
+            list_data.append(row)
+        return list_data
+
+
+def set_():
+    pass
 
 
 def main():
-    pass
+    data_list = read_file()
+    connection = db_connect()
+    cursor = connection.cursor()
+    table = create_table()
+    handler(cursor, table)
 
 
 if __name__ == '__main__':
     main()
-
